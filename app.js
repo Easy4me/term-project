3bf0bb9ebbfbc62e8ba2ded3221861b1cbd8a6fd
Manuel <Manuel@sfs-wifi-dhcp-10-143-5-234.wifi.sfsu.edu>
2025-05-01 17:36:14 -0700

    add view for server and ejs dependency

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Tell Express to use EJS
+app.set('view engine', 'ejs');
+
+// Optional: Set the directory for view templates
+app.set('views', './views');
+
 // Simple test route
 app.get('/', (req, res) => {
   res.send('Welcome to the E-Commerce App');
