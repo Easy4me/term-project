d9030114ed3655019cc303b8023b6b2809835b6f
John Price <jrprice27@gmail.com>
2025-05-20 10:50:28 -0700

    got pug.card to render everything in cars.json on index.pug... product.pug still does not work

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cars = require('./data/cars.json');
 
 // use pug
 app.set('view engine', 'pug');
@@ -12,7 +13,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // index.pug is the root page
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { cars });
+});
+
+app.get('/product/:shortName', (req, res) => {
+  const car = cars.find(c => c.shortName === req.params.shortName);
+  if (!car) {
+    return res.status(404).send('404: product not found!');
+  }
+  res.render('product', { car });
 });
 
 // Start server
